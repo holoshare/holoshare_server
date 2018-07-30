7997e8fffd3b03ef37af22279317fe917c4f224e
Taranvir Waraich <taranvir@me.com>
2018-07-30 04:05:34 +0000

    Designed a simple protocol #15

--- lib/holoshare_server/message_handler.ex
+++ lib/holoshare_server/message_handler.ex
@@ -2,9 +2,10 @@ defmodule HoloshareServer.MessageHandler do
   use GenServer
   require Logger
 
-  alias HoloshareServer.Session.SessionSupervisor
+  alias HoloshareServer.SessionSupervisor
   alias HoloshareServer.SharedSession
   alias HoloshareServer.UDPServer
+  alias HoloshareServer.Helpers
 
 
   def start_link([{:udp, pid} | args]) do
@@ -23,7 +24,7 @@ defmodule HoloshareServer.MessageHandler do
 
   def handle_cast({:recv_message, ip, port, obj}, state) when is_binary(obj) do
     case Poison.decode(obj) do
-      {:ok, obj} -> handle_message(ip, port, obj, state)
+      {:ok, obj} -> handle_message(ip, port, Helpers.safe_atomize_keys(obj), state)
       {:error, error} ->
         Logger.error "UDP Payload parse error ... Poison.decode error"
         Logger.error inspect(error)
@@ -36,13 +37,27 @@ defmodule HoloshareServer.MessageHandler do
     {:noreply, state}
   end
 
-  defp handle_message(ip, port, %{"type" => "INIT"}, state) do
+  defp handle_message(ip, port, %{type: "INIT"} = msg, state) do
+    session_pid = SessionSupervisor.get_session(msg[:session_id])
+    session = case session_pid do 
+      nil -> nil
+      pid -> SharedSession.get_session(pid)
+    end
     UDPServer.send_message(
       state[:udp_pid],
       ip,
       port,
-      Poison.encode! %{type: "resp", text: "HELLO"}
+      Poison.encode! %{type: "FULL_SESSION", session: session}
     )
+  end
+
+  defp handle_message(ip, port, %{type: "ACTION"} = msg, state) do
+    if SessionSupervisor.session_exists?(msg[:session_id]) do
+      session_pid = SessionSupervisor.get_session(msg[:session_id])
+      result = SharedSession.preform_action(session_pid, msg[:data])
+      UDPServer.broadcast(state[:udp_pid], Poison.encode!(%{data: result, type: "CHANGE"}), [{ip, port}])
+    end
+    SessionSupervisor.get_session(msg[:session_id])
   end
 
   defp handle_message(_ip, _port, msg, _state) do
